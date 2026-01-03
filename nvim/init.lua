126dda75b3948c6d26391b417eda9e69c5c7d5a5
Pratap Karonde <pratap.karonde@gmail.com>
2026-01-03 17:13:50 -0600

    format init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,83 +1,83 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
-    -- Your current options
-    change_detection = {
-        notify = false,
-    },
+	-- Your current options
+	change_detection = {
+		notify = false,
+	},
 
-    -- Advanced options you can add:
-    root = vim.fn.stdpath("data") .. "/lazy", -- directory where plugins will be installed
-    defaults = {
-        lazy = false, -- should plugins be lazy-loaded by default?
-        version = nil, -- always use the latest git commit
-        cond = nil, -- condition function to disable plugins
-    },
-    install = {
-        missing = true, -- install missing plugins on startup
-        colorscheme = { "oxocarbon" }, -- colorscheme to use during installation
-    },
-    checker = {
-        enabled = true, -- automatically check for plugin updates
-        notify = true, -- get a notification when new updates are found
-        frequency = 3600, -- check for updates every hour
-    },
-    performance = {
-        cache = {
-            enabled = true,
-        },
-        reset_packpath = true, -- reset the package path to improve startup time
-        rtp = {
-            reset = true, -- reset the runtime path to improve startup time
-            paths = {}, -- add any custom paths here that you want to includes in the rtp
-            disabled_plugins = {
-                "gzip",
-                "matchit",
-                "matchparen",
-                "netrwPlugin",
-                "tarPlugin",
-                "tohtml",
-                "tutor",
-                "zipPlugin",
-            },
-        },
-    },
-    ui = {
-        size = { width = 0.8, height = 0.8 }, -- lazy.nvim UI size
-        wrap = true, -- wrap the lines in the ui
-        border = "none", -- border style: none, single, double, rounded, solid, shadow
-        title = nil, -- title for the ui
-        title_pos = "center", -- title position: center, left, right
-        pills = true, -- show pills in the top of the ui
-        icons = {
-            cmd = "⌘",
-            config = "🛠",
-            event = "📅",
-            ft = "📂",
-            init = "⚙",
-            keys = "🗝",
-            plugin = "🔌",
-            runtime = "💻",
-            require = "🌙",
-            source = "📄",
-            start = "🚀",
-            task = "📋",
-            lazy = "💤",
-        },
-    },
+	-- Advanced options you can add:
+	root = vim.fn.stdpath("data") .. "/lazy", -- directory where plugins will be installed
+	defaults = {
+		lazy = false, -- should plugins be lazy-loaded by default?
+		version = nil, -- always use the latest git commit
+		cond = nil, -- condition function to disable plugins
+	},
+	install = {
+		missing = true, -- install missing plugins on startup
+		colorscheme = { "oxocarbon" }, -- colorscheme to use during installation
+	},
+	checker = {
+		enabled = true, -- automatically check for plugin updates
+		notify = true, -- get a notification when new updates are found
+		frequency = 3600, -- check for updates every hour
+	},
+	performance = {
+		cache = {
+			enabled = true,
+		},
+		reset_packpath = true, -- reset the package path to improve startup time
+		rtp = {
+			reset = true, -- reset the runtime path to improve startup time
+			paths = {}, -- add any custom paths here that you want to includes in the rtp
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				"matchparen",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+	ui = {
+		size = { width = 0.8, height = 0.8 }, -- lazy.nvim UI size
+		wrap = true, -- wrap the lines in the ui
+		border = "none", -- border style: none, single, double, rounded, solid, shadow
+		title = nil, -- title for the ui
+		title_pos = "center", -- title position: center, left, right
+		pills = true, -- show pills in the top of the ui
+		icons = {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			require = "🌙",
+			source = "📄",
+			start = "🚀",
+			task = "📋",
+			lazy = "💤",
+		},
+	},
 })
 
 vim.cmd("set relativenumber")
@@ -91,10 +91,10 @@ vim.cmd("set incsearch")
 --- Enable Diagnostic Message display
 vim.diagnostic.enable = true
 vim.diagnostic.config({
-    --- virtual_lines = true,
-    virtual_text = true, --- show inline message
-    signs = true,        --- show signs in the gutter
-    underline = true,    --- undlerline problematic text
-    update_in_insert = false, --- don't update diagnostic when inserting
-    severity_sort = true,--- sort messages by severity
+	--- virtual_lines = true,
+	virtual_text = true, --- show inline message
+	signs = true, --- show signs in the gutter
+	underline = true, --- undlerline problematic text
+	update_in_insert = false, --- don't update diagnostic when inserting
+	severity_sort = true, --- sort messages by severity
 })
